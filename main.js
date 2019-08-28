36745c3c7d4100af0e625a1c8bdc2c57536d630d
amenpunk <ondasycircuitos@gmail.com>
2019-08-28 12:55:42 -0600

    button nav bar :smile:

--- main.js
+++ main.js
@@ -116,3 +116,12 @@ $('document').ready(function(){
     });
         
 });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
